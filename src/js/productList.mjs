ec6225d0e04da3e3deb76455f9225c071ca48951
obazeec <obazee.esosa@yahoo.com>
2025-05-16 19:31:36 +0100

    Individual activity, dynamci product list

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,5 +1,17 @@
+ import {renderListWithTemplate } from "./utils.mjs"
+ function productCardTemplate(product){
+    return `
+        <li class="product-card">
+        <a href="product_pages/?product=" >
+        <img src="" alt ="Image of ">
+        <h2 class="card__brand"></h2>
+        <h3 class="card__name"></h3>
+        <p class="product-card__price">$</p>
+        </a>
+        </li>
+        `;
 
-
+}
 export default class ProductList{
     constructor(category,dataSource, listElement){
         this.category = category;
@@ -9,6 +21,15 @@ export default class ProductList{
     
     async init(){
         const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list){
+      const htmlStrings = list.map(productCardTemplate);
+      this.listElement.insertAdjacentHTML("afterbegin", htmlString.join(""));
+
+     // apply use new utility function instead of the commented code above
+     //renderListWithTemplate(productCardTemplate, this.listElement, list);
     }
 
 }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -26,6 +26,14 @@ export function getParam(param){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get("product");
-
   return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false){
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear the contents of the parent.
+  if(clear){
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
