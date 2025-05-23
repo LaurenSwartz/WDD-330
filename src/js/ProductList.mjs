84c030bbb365ce72ac0314e7458306bcaa0f0e24
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-05-23 09:41:17 -0300

    modified:   src/js/ProductList.mjs 	renamed:    src/js/main.js -> src/js/product-listing.js 	modified:   src/product_list/index.html

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,16 +2,32 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   const imageUrls = Object.values(product.Images).flat();
-  return `
-    <li class="product-card">
-      <a href="../product_pages/index.html?product=${product.Id}&category=${product.Category}">
-        <img src="${imageUrls[1]}" alt="${product.Name}">
-        <h2>${product.Brand.Name}</h2>
-        <h3>${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-      </a>
-    </li>
-    `;
+  if (product.FinalPrice < product.SuggestedRetailPrice){
+     return `
+        <li class="product-card">
+          <a href="../product_pages/index.html?product=${product.Id}&category=${product.Category}">
+            <img src="${imageUrls[1]}" alt="${product.Name}">
+            <h2>${product.Brand.Name}</h2>
+            <h3>${product.Name}</h3>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+            <p class="product-card__price">discount of: $${Number((product.SuggestedRetailPrice - product.FinalPrice).toPrecision(4))}</p>
+          </a>
+        </li>
+    `; 
+  }
+  else {
+    return `
+      <li class="product-card">
+        <a href="../product_pages/index.html?product=${product.Id}&category=${product.Category}">
+          <img src="${imageUrls[1]}" alt="${product.Name}">
+          <h2>${product.Brand.Name}</h2>
+          <h3>${product.Name}</h3>
+          <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+      </li>
+      `;
+  }
+  
 }
 
 export default class ProductList {
@@ -23,6 +39,7 @@ export default class ProductList {
 
   async init() {
     const list = await this.dataSource.getData(this.category);
+    console.log(list);
     this.renderList(list);
   }
 
