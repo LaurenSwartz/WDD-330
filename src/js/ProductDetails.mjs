fbc87d2cb1b03dd3b18381cbf92883d83017c065
LaurenSwartz <swa21010@byui.edu>
2025-05-30 09:44:59 +0200

    export class changed to ProductDetails

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,5 +1,6 @@
-import { setLocalStorage} from "./utils.mjs";
-export default class ProductDetail{
+import {setLocalStorage} from "./utils.mjs";
+
+export default class ProductDetails{
     constructor(productId, dataSource){
         this.productId = productId;
         this.product = {};
